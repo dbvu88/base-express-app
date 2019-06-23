fe65367402cd51b24c698ee3d1c2e07854b59ca5
Duc Vu <de.yd315@gmail.com>
2019-06-23 17:32:24 +0700

    added application

---
+++ index.js
@@ -0,0 +1,10 @@
+require('rootpath')();
+require('dotenv').config;
+
+const server = require('server/')
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+server.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
